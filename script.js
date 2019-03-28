e70869761818de2ba26cd69cb6149e86b99581a3
Agnieszka Matyja <agmatyja@gmail.com>
2019-03-28 23:23:27 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch(instruction) {
+            case '/exit': 
+				process.stdout.write('Quitting app!\n');
+				process.exit()
+				break;
+			case '/version': 
+				process.stdout.write('Version:\t' + process.versions.node + '\nLanguage:\t' + process.env.LANG + '\n');
+				break;	
+			default: 
+				process.stdout.write('Wrong instruction!\n');
+				break;
+        }
+    }
+});
