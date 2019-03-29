05953d4f0de855d873bf9792d294ba725217cc32
Agnieszka Matyja <agmatyja@gmail.com>
2019-03-29 20:12:52 +0100

    good-looking code

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', function() {
-    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
-    var input = process.stdin.read();
-    if (input !== null) {
-        var instruction = input.toString().trim();
-        switch(instruction) {
-            case '/exit': 
+	var input = process.stdin.read();
+	if (input !== null) {
+		var instruction = input.toString().trim();
+		switch(instruction) {
+			case '/exit': 
 				process.stdout.write('Quitting app!\n');
 				process.exit()
 				break;
@@ -16,6 +15,6 @@ process.stdin.on('readable', function() {
 			default: 
 				process.stdout.write('Wrong instruction!\n');
 				break;
-        }
-    }
+		}
+	}
 });
